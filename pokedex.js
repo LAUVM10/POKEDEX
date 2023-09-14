072012372a993cd4785325f5138e4bdb1e7e21d6
LAUVM10 <ing.lauvm@gmail.com>
2023-09-13 21:31:33 -0500

    save changes

--- pokedex.js
+++ pokedex.js
@@ -1,2 +1,67 @@
-console.log('JS ready');
-alert("Hola mundo");
+const pokemonListElement = document.getElementById("pokemonList");
+const modalElement = document.getElementById("modal");
+const searchInput = document.getElementById("searchInput");
+
+// Fetch the Pokemon data from the JSON file
+fetch("https://storage.googleapis.com/campus-cvs/00000000000-images-lectures/pokemons.json")
+    .then(response => response.json())
+    .then(data => {
+        // Function to create a Pokemon card
+        function createPokemonCard(pokemon) {
+            const card = document.createElement("div");
+            card.classList.add("pokemon-card");
+
+            card.innerHTML = `
+                <img src="${pokemon.image}" alt="${pokemon.name}">
+                <h2>${pokemon.name}</h2>
+                <p>Type: ${pokemon.type}</p>
+            `;
+
+            // Add a click event to show the modal with more information
+            card.addEventListener("click", () => {
+                showPokemonDetails(pokemon);
+            });
+
+            return card;
+        }
+
+        // Function to show the modal with Pokemon details
+        function showPokemonDetails(pokemon) {
+            modalElement.innerHTML = `
+                <div id="modal-content">
+                    <h2>${pokemon.name}</h2>
+                    <p>Type: ${pokemon.type}</p>
+                    <p>Weight: ${pokemon.weight}</p>
+                    <p>Movements:</p>
+                    <ul>
+                        ${pokemon.movements.map(move => `<li>${move}</li>`).join("")}
+                    </ul>
+                </div>
+            `;
+
+            modalElement.style.display = "flex";
+        }
+
+        // Render the list of Pokemon cards
+        data.forEach(pokemon => {
+            const card = createPokemonCard(pokemon);
+            pokemonListElement.appendChild(card);
+        });
+
+        // Add search functionality
+        searchInput.addEventListener("input", () => {
+            const searchValue = searchInput.value.toLowerCase();
+            const cards = document.querySelectorAll(".pokemon-card");
+
+            cards.forEach(card => {
+                const name = card.querySelector("h2").textContent.toLowerCase();
+                if (name.includes(searchValue)) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
+            });
+        });
+    })
+    .catch(error => console.error(error));
+
